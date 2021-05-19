5fb7a1d1c92719f3d28946c895f4bd075de82e44
mwszekely <19625841+mwszekely@users.noreply.github.com>
2021-05-18 22:57:31 -0400

    Minor cleanup

--- src/provide-id.tsx
+++ src/provide-id.tsx
@@ -1,7 +1,6 @@
 
 import { ComponentChildren, createContext, h } from "preact";
 import { useContext, useRef, useState } from "preact/hooks"
-//import { v4 as uuidv4 } from "uuid"
 
 const HexDigits = "0123456789ABCDEF".split("")
 function G() { return HexDigits[Math.floor(Math.random() * 15)]; }
@@ -14,28 +13,19 @@ function fakeUuid() {
 // If inside a component that provides a random ID, returns that ID that can be used for, say, a <label>.
 // If outside, generates a random ID that can then be used for a context for the above.
 function useRandomId() {
-    //const idFromContext = useContext(RandomIdContext);
     const [uuid, _] = useState(() => fakeUuid());
-
-    //if (idFromContext)
-    //    return idFromContext;
-
     return uuid;
 }
 
 export function useProvidedId(allowBackup: "backup" | "no-backup", idFromProp: string | undefined | null): string | undefined {
     let idFromContext = useContext(RandomIdContext);
     let backupId = useRandomId();
-    return idFromProp ?? idFromContext ?? (allowBackup == "backup"? backupId : undefined);
+    return idFromProp || idFromContext || (allowBackup == "backup" ? backupId : undefined);
 }
-
-
-
-
 
 const RandomIdContext = createContext("");
 
 export function ProvideId({ children, id }: { children: ComponentChildren; id?: string }) {
     const backupId = useRandomId();
-    return <RandomIdContext.Provider value={id ?? backupId}>{children}</RandomIdContext.Provider>
+    return <RandomIdContext.Provider value={id || backupId}>{children}</RandomIdContext.Provider>
 }
