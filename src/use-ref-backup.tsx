5bc82b058832701ffebe897040f23bda413ce52a
mwszekely <19625841+mwszekely@users.noreply.github.com>
2021-05-22 19:24:50 -0400

    Fixed ref backup function types

--- src/use-ref-backup.tsx
+++ src/use-ref-backup.tsx
@@ -1,10 +1,10 @@
-import { Ref } from "preact";
+import { Ref, RefCallback } from "preact";
 import { useRefElement } from "./use-ref-element";
 
-export function useRefBackup<E extends HTMLElement>(givenRef?: Ref<E>) {
+export function useRefBackup<E extends HTMLElement>(givenRef?: Ref<E>): RefCallback<E> {
     return useRefBackupProps({ ref: givenRef }).ref;
 }
 
 export function useRefBackupProps<P extends { ref?: Ref<any> }>(p: P) {
-    return useRefElement<P>().useRefElementProps(p);
+    return useRefElement<P extends { ref?: Ref<infer T> }? T : HTMLElement>().useRefElementProps(p);
 }
